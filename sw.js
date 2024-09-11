b334cbc372082d41cc1b7f398cdbe721e391b30f
c0ld1nk <mouse23333@163.com>
2024-09-11 23:22:07 +0800

    Site updated: 2024-09-11 23:22:07

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1726067898268";
+const VERSION = "1726068127331";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
